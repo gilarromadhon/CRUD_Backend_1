959e9bf406dd04ae90b086295c49e7c1441db66b
Gilar Romadhon <gilarromadhon@gmail.com>
2022-11-16 21:43:08 +0700

    percobaan

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
-import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
+// import cors from "cors";
+// import UserRoute from "./routes/UserRoute.js";
  
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(UserRoute);
+ 
+// app.listen(5001, ()=> console.log('Server up and running...'));
+
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(UserRoute);
- 
-app.listen(5001, ()=> console.log('Server up and running...'));
+const port = 9000;
+app.use("/", (req, res) => {
+    res.json({
+        message: "Hello"
+    })
+})
+app.listen(9000, () => {
+    console.log(`Server up and running on ${port}`)
+});
