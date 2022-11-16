3ec17a8f142e72c66d488916dad757efe5fc34b0
Gilar Romadhon <gilarromadhon@gmail.com>
2022-11-16 21:52:14 +0700

    hmm

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
+// import cors from "cors";
+// import UserRoute from "./routes/UserRoute.js";
  
 const app = express();
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 // app.use(UserRoute);
  
 app.use("/", (req, res) => {
@@ -13,4 +13,6 @@ app.use("/", (req, res) => {
     })
 })
 
-app.listen(5001, () => console.log('Server up and running...'));
+app.listen(5000, () => {
+    console.log('Server up and running...')
+});
